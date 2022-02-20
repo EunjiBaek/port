5067acccf2e54c405d031310ab4bde0e7d4ceb7f
EunjiBaek <ejbaek@k-auction.com>
2022-02-20 18:38:20 +0900

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
         $(".skill_graph span b").animate({"opacity":"1"},1000);
     }, 2000);
 
+
+    var swiper = new Swiper(".effectSwiper", {
+        cssMode: true,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+    });
+
 });
 
 const cursor = document.querySelector(".cursor");
@@ -17,15 +32,6 @@ const follower = document.querySelector(".cursor-follower");
 
 //마우스 움직이기
 window.addEventListener("mousemove", function(e){
-
-    //마우스 좌표 값
-    let pageX = e.pageX;
-    let pageY = e.pageY;
-
-    //기준점 가운데로 변경
-    let standardX = window.innerWidth/2 - pageX;
-    let standardY = window.innerHeight/2 - pageY;
-
 
     gsap.to(cursor, {duration: 0.3, left: e.pageX - 5, top: e.pageY - 5 });
     gsap.to(follower, {duration: 0.8, left: e.pageX - 15, top: e.pageY - 15 });
@@ -41,13 +47,8 @@ window.addEventListener("mousemove", function(e){
         });
     });
 
-     //이미지 움직임
-     const move = document.querySelector(".effect_wrap > div > .img img");
-     move.style.transform = "translate(" + standardX/20 + "px," + standardY/20 + "px)";
-
-
-     
-
 });
+
+
 
 
